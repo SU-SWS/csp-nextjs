7491967a1e2a7f16d4e482035a497a745427ada1
Jen Breese-Kauth <jbreese@stanford.edu>
2026-06-30 08:23:58 -0700

    fixup to padding

--- src/components/config-pages/quarter-alert.tsx
+++ src/components/config-pages/quarter-alert.tsx
@@ -25,14 +25,14 @@ const QuarterAlert = async ({className}: Props) => {
     <>
       <div
         className={twMerge(
-          "rs-px-2 mx-[3rem] mb-[.9rem] gap-8 rounded-3xl border-2 py-[.9rem] pr-32 leading-none text-white xl:rs-pt-0 xl:rs-pb-2 xl:rs-py-2 xl:rs-px-2 md:mx-24 lg:mr-24 lg:max-w-[65rem] lg:justify-self-end xl:block xl:max-w-[24rem] xl:rounded-b-3xl xl:rounded-t-none xl:border-t-0 xl:text-left",
+          "rs-px-2 mx-[3rem] mb-[.9rem] gap-8 rounded-3xl border-2 py-[.9rem] pr-32 leading-none text-white xl:rs-pt-0 xl:rs-pb-1 xl:rs-px-1 md:mx-24 lg:mr-24 lg:justify-self-end xl:block xl:max-w-[24rem] xl:rounded-b-3xl xl:rounded-t-none xl:border-t-0 xl:text-left",
           bgClass,
           className
         )}
       >
-        <div className="">
+        <div className="flex flex-row items-center gap-8 xl:block">
           {config.cspQaLabel && (
-            <h2 className="xl-mb-0 type-0 mb-0 font-normal uppercase leading-none xl:leading-normal">
+            <h2 className="xl-mb-0 mb-0 text-18 font-normal uppercase leading-none xl:leading-normal">
               {config.cspQaLabel}
             </h2>
           )}
