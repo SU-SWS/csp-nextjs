4183d3021595cc736c8d7dfb975de3acad75eaa5
Jen Breese-Kauth <jbreese@stanford.edu>
2026-07-13 17:43:25 -0700

    CSP-114: updating styles

--- src/components/menu/side-nav.tsx
+++ src/components/menu/side-nav.tsx
@@ -1,6 +1,7 @@
 import Link from "@components/elements/link"
 import {BookLink, MenuItem as MenuItemType} from "@lib/gql/__generated__/graphql"
 import {HTMLAttributes} from "react"
+import {ChevronDownIcon} from "@heroicons/react/20/solid"
 import cn from "@lib/utils/className"
 
 type Props = HTMLAttributes<HTMLElement> & {
@@ -17,7 +18,7 @@ type Props = HTMLAttributes<HTMLElement> & {
 const SideNav = ({menuItems, activeTrail, ...props}: Props) => {
   return (
     <nav aria-label="Secondary Navigation" {...props}>
-      <ul className="list-unstyled">
+      <ul className="list-unstyled mb-20">
         {menuItems.map(item => (
           <MenuItem key={`sidenav--${item.id}`} {...item} activeTrail={activeTrail} level={0} />
         ))}
@@ -32,21 +33,30 @@ type MenuItemProps = (MenuItemType | BookLink) & {
 }
 
 const MenuItem = ({id, url, title, children, activeTrail, level, expanded}: MenuItemProps) => {
-  const linkClasses = cn("relative inline-block w-full py-5 pl-10 font-normal no-underline hocus:underline", {
-    // Non-active state.
-    "text-digital-red before:scale-y-[1] before:transition hocus:text-black hocus:before:absolute hocus:before:left-0 hocus:before:top-0 hocus:before:block hocus:before:h-full hocus:before:w-[6px] hocus:before:bg-black hocus:before:content-['']":
-      activeTrail.at(-1) !== id,
-    // Active state.
-    "text-black before:absolute before:left-0 before:top-0 before:block before:h-full before:w-[6px] before:bg-black before:content-['']":
-      activeTrail.at(-1) === id,
-  })
+  const hasChildren = !!children && children.length > 0
+  const isOpen = expanded && hasChildren && activeTrail.includes(id)
+
+  const linkClasses = cn(
+    "group relative flex w-full items-center py-5 pl-10 font-sans text-17 font-normal no-underline hocus:underline",
+    {
+      // Non-active state.
+      "text-digital-red before:scale-y-[1] before:transition hocus:text-archway-dark hocus:before:absolute hocus:before:left-0 hocus:before:top-0 hocus:before:block hocus:before:h-full hocus:before:w-[6px] hocus:before:bg-archway-dark hocus:before:content-['']":
+        activeTrail.at(-1) !== id,
+      // Active state.
+      "text-archway-dark before:absolute before:left-0 before:top-0 before:block before:h-full before:w-[6px] before:bg-archway-dark before:content-['']":
+        activeTrail.at(-1) === id,
+    }
+  )
 
   return (
     <li className="m-0 border-b p-0 last:border-0">
       <Link href={url || "#"} className={linkClasses} aria-current={activeTrail.at(-1) === id ? "page" : undefined}>
         {title}
+        {hasChildren && (
+          <ChevronDownIcon height={20} aria-hidden="true" className="ml-auto shrink-0 text-archway-light" />
+        )}
       </Link>
-      {expanded && children && children.length > 0 && activeTrail.includes(id) && (
+      {isOpen && (
         <ul
           className={cn("list-unstyled border-t", {
             "pl-10": level === 0,
