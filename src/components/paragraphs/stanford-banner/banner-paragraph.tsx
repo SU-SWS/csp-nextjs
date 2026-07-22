5085a270fce85c6c4fffbcc210e12b733c97927c
Jen Breese <jbreese@stanford.edu>
2026-07-22 14:21:50 -0700

    CSP-81: Styles for multiple buttons on image banner (#20)
    * CSP-81: Styles for multiple buttons on image banner
    
    * Fixups per design review.
    
    * fixup
    
    * Change button type name
    
    * Corrected the whitespace

--- src/components/paragraphs/stanford-banner/banner-paragraph.tsx
+++ src/components/paragraphs/stanford-banner/banner-paragraph.tsx
@@ -40,26 +40,30 @@ const BannerParagraph = ({paragraph, eagerLoadImage, ...props}: Props) => {
     >
       {hasCard && (
         <>
-          {paragraph.suBannerHeader && (
-            <>
-              {headerTag === "h2" && (
-                <H2 id={id} className={cn(headerClasses, "type-3 mb-0 font-serif font-normal text-csp-cream")}>
-                  {paragraph.suBannerHeader}
-                </H2>
-              )}
-              {headerTag === "h3" && (
-                <H3 id={id} className={headerClasses}>
-                  {paragraph.suBannerHeader}
-                </H3>
-              )}
-              {headerTag === "h4" && (
-                <H4 id={id} className={headerClasses}>
-                  {paragraph.suBannerHeader}
-                </H4>
-              )}
-              {headerTag === "div" && <div className={headerClasses}>{paragraph.suBannerHeader}</div>}
-            </>
-          )}
+          <div className="@6xl:max-w-[600px]">
+            {paragraph.suBannerHeader && (
+              <>
+                {headerTag === "h2" && (
+                  <H2 id={id} className={cn(headerClasses, "type-3 my-0 font-serif font-normal text-csp-cream")}>
+                    {paragraph.suBannerHeader}
+                  </H2>
+                )}
+                {headerTag === "h3" && (
+                  <H3 id={id} className={cn(headerClasses, "my-0 text-csp-cream")}>
+                    {paragraph.suBannerHeader}
+                  </H3>
+                )}
+                {headerTag === "h4" && (
+                  <H4 id={id} className={cn(headerClasses, "my-0 text-csp-cream")}>
+                    {paragraph.suBannerHeader}
+                  </H4>
+                )}
+                {headerTag === "div" && (
+                  <div className={cn(headerClasses, "text-csp-cream")}>{paragraph.suBannerHeader}</div>
+                )}
+              </>
+            )}
+          </div>
 
           {paragraph.suBannerSupHeader && (
             <div className="type-2 order-first font-normal text-fog-dark">{paragraph.suBannerSupHeader}</div>
@@ -70,15 +74,18 @@ const BannerParagraph = ({paragraph, eagerLoadImage, ...props}: Props) => {
             className="type-0 font-normal text-csp-cream [&_p]:!type-0"
           />
 
-          {paragraph.suBannerButton?.map((button, i) => (
-            <Button
-              key={i}
-              href={button.url}
-              variant={button.attributes?.buttonType === "secondary" ? "secondary" : "primary"}
-            >
-              {button.title}
-            </Button>
-          ))}
+          <div className="m-0 columns-1 gap-1 @7xl:columns-3">
+            {paragraph.suBannerButton?.map((button, i) => (
+              <Button
+                key={i}
+                href={button.url}
+                variant={button.attributes?.imageBannerButtonType === "archway" ? "archway" : "digitalred"}
+                className="m-2"
+              >
+                {button.title}
+              </Button>
+            ))}
+          </div>
         </>
       )}
     </HeroBanner>
--- src/components/patterns/hero-banner.tsx
+++ src/components/patterns/hero-banner.tsx
@@ -95,7 +95,7 @@ const HeroBanner = async ({
           className={cn("relative z-[11] flex size-full flex-col gap-10", {
             "cc rs-py-4 items-center justify-center text-center text-white @6xl:max-w-800":
               overlayPosition === "center",
-            "rs-p-2 @6xl:z-10 @6xl:my-24 @6xl:max-w-[600px] @6xl:bg-transparent": overlayPosition !== "center",
+            "rs-p-2 @6xl:z-10 @6xl:my-24 @6xl:max-w-[900px] @6xl:bg-transparent": overlayPosition !== "center",
             "@6xl:ml-auto @6xl:mr-20": overlayPosition === "right",
             "@6xl:ml-20 @6xl:mr-auto": overlayPosition === "left",
           })}
