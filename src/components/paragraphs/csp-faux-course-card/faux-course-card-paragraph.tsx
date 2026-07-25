8c5f41713eb5dd8ec9130dd1bd4283ff67cc6701
Jen Breese-Kauth <jbreese@stanford.edu>
2026-07-24 17:01:13 -0700

    fixup

--- src/components/paragraphs/csp-faux-course-card/faux-course-card-paragraph.tsx
+++ src/components/paragraphs/csp-faux-course-card/faux-course-card-paragraph.tsx
@@ -32,7 +32,7 @@ const FauxCourseCardParagraph = ({paragraph, ...props}: Props) => {
       )}
     >
       {image?.url && (
-        <div className="relative m-[.6rem] aspect-[54/29] w-full flex-grow self-start overflow-hidden rounded-csp-sm @9xl:min-w-[56rem]">
+        <div className="relative mx-[.6rem] mt-[.6rem] aspect-[54/29] max-w-[calc(100%_-_1.2rem)] flex-grow self-start overflow-hidden rounded-csp-sm @9xl:min-w-[56rem]">
           {url ? (
             <Link href={url} className="relative block h-full">
               <Image src={image.url} alt={image.alt || ""} fill className="border-transparent object-cover" />
